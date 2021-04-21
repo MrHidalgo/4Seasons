c69a75402436ece84f47b2879faa9335a5055919
Andrii <Luckyhidalgo@gmail.com>
2021-04-21 11:20:37 +0300

    Home page :::  - created gallery section;  - fixed main section visual bg;  - fixed reviews header btn position & indents;

--- dest/js/app.js
+++ dest/js/app.js
@@ -113,13 +113,19 @@ var initSwiper = function initSwiper() {
   $('.reviews__slider').css({ paddingLeft: $('.reviews__title')[0].getBoundingClientRect().left });
   $('.reviews__bg').css({ width: $('.reviews__btn--next')[0].getBoundingClientRect().right + 50 });
 
+  // GALLERY
+  $('.gallery__slider').css({ paddingLeft: $('.gallery__title')[0].getBoundingClientRect().left });
+  $('.gallery__bg').css({ width: $('.gallery__btn--next')[0].getBoundingClientRect().right + 50 });
+
   $(window).on('resize', function () {
+    $('.gallery__slider').css({ paddingLeft: $('.gallery__title')[0].getBoundingClientRect().left });
+    $('.gallery__bg').css({ width: $('.gallery__btn--next')[0].getBoundingClientRect().right + 50 });
+
     $('.reviews__slider').css({ paddingLeft: $('.reviews__title')[0].getBoundingClientRect().left });
     $('.reviews__bg').css({ width: $('.reviews__btn--next')[0].getBoundingClientRect().right + 50 });
   });
-  // end :: REVIEWS
-  /* ========== */
 
+  // start :: REVIEWS
   new Swiper('.reviewsSwiper', {
     loop: false,
     effect: 'slide',
@@ -138,6 +144,30 @@ var initSwiper = function initSwiper() {
       }
     }
   });
+  // end :: REVIEWS
+  /* ========== */
+
+  // start :: GALLERY
+  new Swiper('.gallerySwiper', {
+    loop: false,
+    effect: 'slide',
+    speed: 1000,
+    slidesPerView: 'auto',
+    spaceBetween: 20,
+    slidesOffsetAfter: 20,
+    navigation: {
+      nextEl: '.gallery__btn--next',
+      prevEl: '.gallery__btn--prev'
+    },
+    on: {
+      init: function init(swiper) {
+        $(swiper.$el).animate({ opacity: 1 }, 550);
+        $('.gallery__bg').animate({ opacity: 1 }, 550);
+      }
+    }
+  });
+  // end :: GALLERY
+  /* ========== */
 };
 
 /**
--- src/js/_lib/swiper.js
+++ src/js/_lib/swiper.js
@@ -13,14 +13,19 @@ const initSwiper = () => {
   $('.reviews__slider').css({paddingLeft: $('.reviews__title')[0].getBoundingClientRect().left});
   $('.reviews__bg').css({width: $('.reviews__btn--next')[0].getBoundingClientRect().right + 50});
   
+  // GALLERY
+  $('.gallery__slider').css({paddingLeft: $('.gallery__title')[0].getBoundingClientRect().left});
+  $('.gallery__bg').css({width: $('.gallery__btn--next')[0].getBoundingClientRect().right + 50});
+  
   $(window).on('resize', () => {
+    $('.gallery__slider').css({paddingLeft: $('.gallery__title')[0].getBoundingClientRect().left});
+    $('.gallery__bg').css({width: $('.gallery__btn--next')[0].getBoundingClientRect().right + 50});
+    
     $('.reviews__slider').css({paddingLeft: $('.reviews__title')[0].getBoundingClientRect().left});
     $('.reviews__bg').css({width: $('.reviews__btn--next')[0].getBoundingClientRect().right + 50});
   });
-  // end :: REVIEWS
-  /* ========== */
-
   
+  // start :: REVIEWS
   new Swiper('.reviewsSwiper', {
     loop: false,
     effect: 'slide',
@@ -39,4 +44,28 @@ const initSwiper = () => {
       }
     }
   });
+  // end :: REVIEWS
+  /* ========== */
+  
+  // start :: GALLERY
+  new Swiper('.gallerySwiper', {
+    loop: false,
+    effect: 'slide',
+    speed: 1000,
+    slidesPerView: 'auto',
+    spaceBetween: 20,
+    slidesOffsetAfter: 20,
+    navigation: {
+      nextEl: '.gallery__btn--next',
+      prevEl: '.gallery__btn--prev',
+    },
+    on: {
+      init: function(swiper) {
+        $(swiper.$el).animate({opacity: 1}, 550);
+        $('.gallery__bg').animate({opacity: 1}, 550)
+      }
+    }
+  });
+  // end :: GALLERY
+  /* ========== */
 };
