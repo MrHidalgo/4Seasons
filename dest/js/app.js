7ae7f8b9d5a4e3ea1be55c2a53845b4e975999b7
Andrii <Luckyhidalgo@gmail.com>
2021-04-26 12:37:26 +0300

    Home page :::  - added animation;

--- dest/js/app.js
+++ dest/js/app.js
@@ -309,6 +309,42 @@ window.addEventListener('scroll', function (ev) {
       $('[count-js]').val(Number($('[count-js]').val()) + 1);
     });
   };
+
+  var initPathFloatingAnimation = function initPathFloatingAnimation() {
+    var xMin = -25,
+        xMax = 25,
+        yMin = -50,
+        yMax = 50,
+        positionsPerElement = 5,
+        secondsPerIteration = 5,
+        elements = $("[floating-node-js]");
+
+    for (var i = 0; i < elements.length; i++) {
+      randomFloat(elements[i], positionsPerElement, secondsPerIteration);
+    }
+
+    function random(min, max) {
+      return min + Math.random() * (max - min);
+    }
+
+    function randomFloat(element, positions, duration) {
+      var tl = new TimelineMax({
+        repeat: -1,
+        yoyo: true,
+        delay: Math.random() * duration
+      });
+
+      for (var _i = 0; _i < positions; _i++) {
+        tl.to(element, duration, {
+          x: random(xMin, xMax),
+          y: random(yMin, yMax),
+          ease: Sine.easeInOut
+        });
+      }
+
+      return tl;
+    }
+  };
   /*
   * CALLBACK :: end
   * ============================================= */
@@ -331,6 +367,7 @@ window.addEventListener('scroll', function (ev) {
     // callback
     searchCB();
     countCB();
+    initPathFloatingAnimation();
     // ==========================================
   };
   initNative();
